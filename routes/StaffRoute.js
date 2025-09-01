725ff7f39a55db4030dea64411cd4c4b701d3d15
sumit7654 <129644429+sumit7654@users.noreply.github.com>
2025-09-01 22:59:19 +0530

    commited

--- routes/StaffRoute.js
+++ routes/StaffRoute.js
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post("/register", registerStaffController);
 router.post("/login", loginStaffController);
 router.get("/dashboard-stats", getDashboardStatsController);
-router.get("/todays-deliveries/:staffId", getTodaysDeliveriesController);
+router.get("/todays-deliveries/:deliveryBoyId", getTodaysDeliveriesController);
 router.get("/unassigned-deliveries", getUnassignedDeliveriesController);
 
 export default router;

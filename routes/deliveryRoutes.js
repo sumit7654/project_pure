fe40347318c96cbf4bc92569ed78b4e67be47ab2
sumit7654 <129644429+sumit7654@users.noreply.github.com>
2025-09-03 01:53:35 +0530

    commited

--- routes/deliveryRoutes.js
+++ routes/deliveryRoutes.js
@@ -1,16 +1,12 @@
 // routes/deliveryRoutes.js
 
 import express from "express";
-import {
-  createDeliveryController,
-  markAsDeliveredController,
-} from "../controller/DeliveryController.js";
+import { markAsDeliveredController } from "../controller/DeliveryController.js";
 
 const router = express.Router();
 
 // Ek specific delivery ko 'Delivered' mark karne ke liye
 // PUT /api/deliveries/:deliveryId/complete
-router.post("/", createDeliveryController);
 router.put("/:deliveryId/complete", markAsDeliveredController);
 
 export default router;

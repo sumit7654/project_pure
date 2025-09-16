05ff85a5f99b649ae3a60c20a69cc5963a868ea1
sumit7654 <129644429+sumit7654@users.noreply.github.com>
2025-09-16 16:50:34 +0530

    commited

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -4,6 +4,7 @@ import ProductModel from "../model/ProductModel.js";
 export const getProductsController = async (req, res) => {
   try {
     const products = await ProductModel.find({});
+    console.log(products);
     res.status(200).json({ success: true, products });
   } catch (error) {
     res

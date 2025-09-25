f050de18b7c301f44c52732c3d1b652514ba2d5b
sumit7654 <129644429+sumit7654@users.noreply.github.com>
2025-09-26 00:29:07 +0530

    commited

--- routes/deliveryRoutes.js
+++ routes/deliveryRoutes.js
@@ -11,6 +11,6 @@ const router = express.Router();
 // Ek specific delivery ko 'Delivered' mark karne ke liye
 // PUT /api/deliveries/:deliveryId/complete
 router.put("/:deliveryId/complete", markAsDeliveredController);
-router.get("/all", getTodaysDeliveriesForAdminController);
+router.get("/today", getTodaysDeliveriesForAdminController);
 
 export default router;

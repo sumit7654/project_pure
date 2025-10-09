767d897b925caa8b676095d25009ad1dba87a19f
sumit7654 <129644429+sumit7654@users.noreply.github.com>
2025-10-10 00:39:07 +0530

    commited

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get("/", (req, res) => {
 
 // Yeh akela, smart job har din subah 1:05 AM IST par chalega
 cron.schedule(
-  "38 0 * * *",
+  "41 0 * * *",
   async () => {
     console.log("--- Starting Daily Subscription Processing Job ---");
     try {
